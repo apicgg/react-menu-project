660097a4125ea7abcb21225a72b7a1017cf3b49f
Anurag Pramanik <p.anurag@outlook.com>
2022-03-31 21:21:33 +0530

    passing data to menu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
-import Menu from './Menu'
-import Categories from './Categories'
-import items from './data'
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
 
 function App() {
-	return <h2>menu project setup</h2>
+  // make your changes here
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        {/* pass "items" imported above here as items prop */}
+        <Menu items={items} />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
